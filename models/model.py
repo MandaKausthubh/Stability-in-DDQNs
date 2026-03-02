87b04031a0edf63a1dc398d8d574ff0d4109e3bd
Manda Kausthubh <manda.kausthubh@gmail.com>
2026-03-02 10:31:08 +0530

    Feature Extraction and DQN design added

--- models/model.py
+++ models/model.py
@@ -1,33 +1,38 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
+class FeatureExtractor(nn.Module):
+    def __init__(self, input_dim, hidden_dims, output_dim):
+        super(FeatureExtractor, self).__init__()
+        self.layers = nn.ModuleList()
+        prev_dim = input_dim
+        for hidden_dim in hidden_dims:
+            self.layers.append(nn.Linear(prev_dim, hidden_dim))
+            self.layers.append(nn.ReLU())
+            prev_dim = hidden_dim
+        self.layers.append(nn.Linear(prev_dim, output_dim))
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
 
 class DQN(nn.Module):
     def __init__(
-        self,
-        input_dim,
-        action_dim,
-        hidden_dims,
+            self,
+            input_dim,
+            hidden_dims,
+            rep_dim,
+            use_bias=True
     ):
         super(DQN, self).__init__()
-        self.layers = nn.ModuleList()
-        prev_dim = input_dim
-
-        for hidden_dim in hidden_dims:
-            self.layers.append(nn.Linear(prev_dim, hidden_dim))
-            prev_dim = hidden_dim
-
-        if hidden_dims: self.layers.append(nn.Linear(prev_dim, action_dim))
-        else: self.layers.append(nn.Linear(input_dim, action_dim))
+        self.feature_extractor = FeatureExtractor(input_dim, hidden_dims, rep_dim)
+        self.weights = nn.Linear(rep_dim, 1, bias=use_bias)
 
     def forward(self, x):
-        for layer in self.layers[:-1]:
-            x = F.relu(layer(x))
-        return self.layers[-1](x)
+        features = self.feature_extractor(x)
+        output = self.weights(features)
+        return output.squeeze()
 
-    def reset_parameters(self):
-        for layer in self.layers:
-            if isinstance(layer, nn.Linear):
-                layer.reset_parameters()
-
-
+    def get_weights(self):
+        return self.weights.weight.data, (self.weights.bias.data if self.weights.bias is not None else None)
